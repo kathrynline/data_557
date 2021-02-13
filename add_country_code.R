6245444f07b02cd63de521c1817a46a222e7db3c
Emily Linebarger <elineb@uw.edu>
2021-02-13 13:29:50 -0800

    Running entire data prep pipeline through run_all script

--- add_country_code.R
+++ add_country_code.R
@@ -1,37 +1,52 @@
-# Relative path for the file to transform. The file must have a "country" column
-originalFilePath <- './raw_data/jhu_deaths_01.26.2021.csv'
-# Name for the country column in the source file
-originalFileCountryColumn <- 'Country.Region'
+# Author: Alison Gale
 
-# Relative path the the resulting file with country code added
-outputFilePath <- './raw_data/jhu_deaths_01.26.2021_with_country_code.csv'
+add_country_codes <- function(file_name, column_name){
+  # Relative path for the file to transform. The file must have a "country" column
+  originalFilePath <- paste0('./raw_data/', file_name)
+  # Name for the country column in the source file
+  originalFileCountryColumn <- column_name
+  
+  # Relative path the the resulting file with country code added
+  outputFilePath <- paste0('./intermediate_data/', file_name)
+  
+  # Read source file and country codes
+  original <-
+    read.csv(file = originalFilePath, fileEncoding = 'UTF-8-BOM')
+  codes <-
+    read.csv(file = './raw_data/country_codes.csv', fileEncoding = 'UTF-8-BOM')
+  
+  
+  # Replace leading white space that appears in country codes
+  codes$country_code <- trimws(codes$Alpha.3.code, which = c("left"))
+  
+  # Rename country column for easier access
+  names(original)[names(original) == originalFileCountryColumn] <- 'country'
+  
+  # Join the two tables on the country column
+  output <-
+    merge(original, codes[c('Country', 'country_code')],
+          by.x = 'country',
+          by.y = 'Country')
+  
+  # Calculate which countries were omitted
+  out_countries <- unique(output$country)
+  in_countries <- unique(original$country)
+  print(paste0('Countries not matched for ', file_name, ':'))
+  print(in_countries[!in_countries %in% out_countries])
+  
+  # Write output to file. This adds a country_codes column and removes all rows
+  # that don't match.
+  write.csv(output, file = outputFilePath, row.names = FALSE)
+} 
 
-# Read source file and country codes
-original <-
-  read.csv(file = originalFilePath, fileEncoding = 'UTF-8-BOM')
-codes <-
-  read.csv(file = './raw_data/country_codes.csv', fileEncoding = 'UTF-8-BOM')
+print("Running add_country_code.R...")
 
+# Call this function once for each of our input files. 
+# If we add raw files, we will need to add them to this list! 
+add_country_codes('ghsi_summary.csv', 'country')
+add_country_codes('island_countries.csv', "country")
+add_country_codes('jhu_cases_01.26.2021.csv', 'Country.Region')
+add_country_codes('jhu_deaths_01.26.2021.csv', 'Country.Region')
+add_country_codes('world_bank_population.csv', 'country')
 
-# Replace leading white space that appears in country codes
-codes$country_code <- trimws(codes$Alpha.3.code, which = c("left"))
-
-# Rename country column for easier access
-names(original)[names(original) == originalFileCountryColumn] <- 'country'
-
-# Join the two tables on the country column
-output <-
-  merge(original, codes[c('Country', 'country_code')],
-        by.x = 'country',
-        by.y = 'Country')
-
-# Calculate which countries were omitted
-out_countries <- unique(output$country)
-in_countries <- unique(original$country)
-print('Countries not matched:')
-print(in_countries[!(in_countries %in% out_countries)])
-
-
-# Write output to file. This adds a country_codes column and removes all rows
-# that don't match.
-write.csv(output, file = outputFilePath, row.names = FALSE)
+print("Add country codes step complete.")
