1dfaf53922d003c9dbb7c59108ff9ba6b6bf9faf
Emily Linebarger <elineb@Emilys-MacBook-Pro.local>
2021-02-28 13:29:28 -0800

    Saving final report pngs

--- final_report_plots_outliers.R
+++ final_report_plots_outliers.R
@@ -6,6 +6,7 @@ library(data.table)
 # Set working directory to root of repository 
 stopifnot(grepl("data_557", getwd()))
 dt = fread("prepped_data/sixmonth.csv")
+outlier_screened = fread("prepped_data/six_month_outlier_screened.csv")
 setwd("final_report_pngs")
 
 png("outlier_initial_hist.png")
@@ -14,4 +15,12 @@ dev.off()
 
 png("outlier_initial_boxplot.png")
 boxplot(dt$cfratio, ylab="Case-fatality ratio")
+dev.off()
+
+png("regression_1_qq_plot.png")
+plot(lm(cfratio ~ overall, data=dt), which=2)
+dev.off()
+
+png("regression_1_qq_plot_post_outliers.png")
+plot(lm(cfratio ~ overall, data=outlier_screened), which=2)
 dev.off()
